46c93b6c85c218413f54b0d529abceda5481b9ee
morteza <morteza.kia8700@gmail.com>
2024-07-24 14:53:06 +0330

    "Removed 'use client' directive and updated empty JSX element syntax"

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,33 @@
+import blog from "@/Model/blogModel";
+import { ConnectToDb } from "@/utils/ConnectToDb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    await ConnectToDb();
+    const { title, content } = await req.json();
+    await blog.create({
+      title,
+      content,
+    });
+    return NextResponse.json({ data: "Profile Created !!!" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { data: "Somethings Went Wront!" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await ConnectToDb();
+    const blogs = await blog.find();
+    return NextResponse.json({ data: blogs }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { data: "Somethings Went Wront!" },
+      { status: 500 }
+    );
+  }
+}
