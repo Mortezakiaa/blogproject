bc1904e0b287185686b862e83525077209e573f6
morteza <morteza.kia8700@gmail.com>
2024-07-26 18:39:59 +0330

    Remove "block max-w-sm" from BlogCard Link component's className.

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -6,7 +6,7 @@ const BlogCard: React.FC<BlogCardProps> = ({ title, content, _id }) => {
   return (
     <Link
       href={`blog/${_id}`}
-      className="block max-w-sm w-[300px] p-6 border group border-gray-200 rounded-lg shadow transition-all hover:bg-gray-100 bg-[#0284c7]"
+      className="w-[300px] p-6 border group border-gray-200 rounded-lg shadow transition-all hover:bg-gray-100 bg-[#0284c7]"
     >
       <h5 className="mb-2 truncate text-2xl font-bold tracking-tight text-white group-hover:text-slate-800">
         {title}
